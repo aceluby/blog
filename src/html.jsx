7da52ab15410b1d68b69a9a35768e9d24366be01
JakeLuby <jake.luby@target.com>
2019-08-09 10:03:57 -0500

    build fixes

--- src/html.jsx
+++ src/html.jsx
@@ -4,16 +4,16 @@
 import React from "react";
 import favicon from "./favicon.png";
 
-let inlinedStyles = "";
-if (process.env.NODE_ENV === "production") {
-  try {
-    /* eslint import/no-webpack-loader-syntax: off */
-    inlinedStyles = require("!raw-loader!../public/styles.css");
-  } catch (e) {
-    /* eslint no-console: "off" */
-    console.log(e);
-  }
-}
+const inlinedStyles = "";
+// if (process.env.NODE_ENV === "production") {
+//   try {
+//     /* eslint import/no-webpack-loader-syntax: off */
+//     inlinedStyles = require("!raw-loader!../public/styles.css");
+//   } catch (e) {
+//     /* eslint no-console: "off" */
+//     console.log(e);
+//   }
+// }
 
 export default class HTML extends React.Component {
   render() {
